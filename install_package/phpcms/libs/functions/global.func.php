f0ee0e71fbe335fc12988033c0c1d1e308607292
Snow <phpcms@msn.com>
2022-08-05 16:31:37 +0800

    引入htmLawed，解决旧版remove_xss误伤，并提升其防御能力

--- install_package/phpcms/libs/functions/global.func.php
+++ install_package/phpcms/libs/functions/global.func.php
@@ -83,30 +83,17 @@ function safe_replace($string) {
  * @param $string
  * @return string
  */
-function remove_xss($string) { 
-    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string);
-
-    $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
-
-    $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
-
-    $parm = array_merge($parm1, $parm2); 
-
-	for ($i = 0; $i < sizeof($parm); $i++) { 
-		$pattern = '/'; 
-		for ($j = 0; $j < strlen($parm[$i]); $j++) { 
-			if ($j > 0) { 
-				$pattern .= '('; 
-				$pattern .= '(&#[x|X]0([9][a][b]);?)?'; 
-				$pattern .= '|(&#0([9][10][13]);?)?'; 
-				$pattern .= ')?'; 
-			}
-			$pattern .= $parm[$i][$j]; 
-		}
-		$pattern .= '/i';
-		$string = preg_replace($pattern, ' ', $string); 
-	}
-	return $string;
+function remove_xss($string, $options = array(), $spec = '') {
+    if (!is_array($string)) {
+        if (!function_exists('htmLawed')) {
+            pc_base::load_sys_func('htmLawed');
+        }
+        return htmLawed($string, array_merge(array('safe' => 1, 'balanced' => 0), $options), $spec);
+    }
+    foreach ($string as $k => $v) {
+        $string[$k] = remove_xss($v, $options, $spec);
+    }
+    return $string;
 }
 
 /**
